3093eeb8bc0ab8cf9f827a8f34a8cce744ee9ae9
Saraphina <saraphinashanell@gmail.com>
2022-07-16 03:06:40 +0200

    Portfolio Commit

---
+++ Javascript/Java.js
@@ -0,0 +1,9 @@
+document.getElementById('contact1').innerHTML="For any enquries please email me and leave a message, call" +
+"<br>or find us on social media platforms.";
+document.getElementById('heading2').innerHTML="Email Me";
+document.getElementById('contact2').innerHTML="Thank you for your input. Will keep in touch with you.";
+document.getElementById('contact3').innerHTML=" <b>Based at :</b><br>" +
+"VandernijlPark<br>" +
+"Andries Potgieter blvd<br>" +
+"Vaal University of Technology<br>"+
+"1900 <br>";
